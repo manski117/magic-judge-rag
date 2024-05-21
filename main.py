329fe356379aa1309ee251d414707fcc5c591b86
krmanski <choromanski117@gmail.com>
2024-05-21 15:36:18 -0400

    Update user message

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Mike"}
+    return {"message": "Hello Future Magic RAG"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
